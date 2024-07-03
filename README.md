1406bb2c6ee6eaa8c1433516ef5d324eb651c3fc
chayleaf <chayleaf-git@pavluk.org>
2024-07-03 19:37:07 +0700

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# osu-wine.nix
+
+This is a version of Wine that uses wine-staging patches as well as some
+other patches that are intended for making the osu! experience better. I
+moved this out from my dotfiles because the amount of patches has
+changed my dotfiles' language displayed in GitHub to "C".
+
+Note that this is *only* meant to be used for osu!, the Wine version may
+be severely outdated, have security vulnerabilities, etc etc. I may
+update this once or twice a year, but no promises.
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    {
+      overlays.default = final: prev: {
+        osu-wine = final.callPackage ./. { };
+      };
+      checks.x86_64-linux = {
+        inherit (self.packages.x86_64-linux) osu-wine;
+      };
+    } // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      formatter = pkgs.nixpkgs-fmt;
+      packages = rec {
+        osu-wine = pkgs.callPackage ./. { };
+        default = osu-wine;
+      };
+    });
+}
